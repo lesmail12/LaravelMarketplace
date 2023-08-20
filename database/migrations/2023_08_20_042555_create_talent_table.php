c56c1d3b5071237540f7ba377ca1e8f7962ef4f3
Lesmail <agungismail444@gmail.com>
2023-08-20 11:42:16 +0700

    first commit

---
+++ database/migrations/2023_08_20_042555_create_talent_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('talent', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('username');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('address');
+            $table->string('phone');
+            $table->string('city');
+            $table->string('country');
+            $table->string('kode_pos');
+            $table->tinyInteger('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('talent');
+    }
+};
